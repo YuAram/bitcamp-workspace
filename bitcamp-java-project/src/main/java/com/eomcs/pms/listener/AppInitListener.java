4d921bcf98d3d05da607c3037442687de0d73138
YuAram <sukgi5319@naver.com>
2020-10-13 17:59:22 +0900

    201013_18:00

--- bitcamp-java-project/src/main/java/com/eomcs/pms/listener/AppInitListener.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/listener/AppInitListener.java
@@ -1,15 +1,17 @@
 package com.eomcs.pms.listener;
 
+import java.util.Map;
 import com.eomcs.context.ApplicationContextListener;
 
 public class AppInitListener implements ApplicationContextListener {
   @Override
-  public void contextInitialized() {
-    System.out.println("프로젝트 관리 시스템(PMS)에 오신 걸 환영합니다!");
+  public void contextInitialized(Map<String,Object> context) {
+    System.out.println("환영합니다~");
   }
 
   @Override
-  public void contextDestroyed() {
-    System.out.println("프로젝트 관리 시스템(PMS)을 종료합니다!");
+  public void contextDestoryed(Map<String,Object> context) {
+    System.out.println("종료합니다!");
   }
 }
+
