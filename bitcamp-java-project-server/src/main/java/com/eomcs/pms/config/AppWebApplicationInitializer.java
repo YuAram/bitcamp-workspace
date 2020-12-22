3888687f7ba64c8b2b1bc2731d613d3f2925041c
YuAram <sukgi5319@naver.com>
2020-12-22 20:11:59 +0900

    201222

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/AppWebApplicationInitializer.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/AppWebApplicationInitializer.java
@@ -4,29 +4,31 @@ import javax.servlet.Filter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppWebApplicationInitializer 
-  extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppWebApplicationInitializer
+extends AbstractAnnotationConfigDispatcherServletInitializer {
 
   @Override
   protected Class<?>[] getRootConfigClasses() {
-    return new Class<?>[] {RootConfig.class, MybatisConfig.class};
+    // ContextLoaderListener 가 사용할 java config 클래스를 리턴한다.
+    return new Class<?>[] {RootConfig.class, DatabaseConfig.class, MybatisConfig.class};
   }
-  
+
   @Override
   protected Class<?>[] getServletConfigClasses() {
+    // DispatcherServlet 이 사용할 java config 클래스를 리턴한다.
     return new Class<?>[] {AppWebConfig.class};
   }
-  
+
   @Override
   protected String[] getServletMappings() {
     return new String[] {"/app/*"};
   }
-  
+
   @Override
   protected String getServletName() {
     return "app";
   }
-  
+
   @Override
   protected Filter[] getServletFilters() {
     return new Filter[] {
--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/AppWebConfig.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/AppWebConfig.java
@@ -1,5 +1,6 @@
 package com.eomcs.pms.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.MultipartResolver;
@@ -11,10 +12,15 @@ import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.util.UrlPathHelper;
+import com.eomcs.pms.service.MemberService;
+import com.eomcs.pms.web.interceptor.AuthInterceptor;
+import com.eomcs.pms.web.interceptor.AutoLoginInterceptor;
 
 @ComponentScan("com.eomcs.pms.web")
 @EnableWebMvc
 public class AppWebConfig implements WebMvcConfigurer {
+
+  @Autowired MemberService memberService;
 
   @Bean
   public ViewResolver viewResolver() {
@@ -29,7 +35,7 @@ public class AppWebConfig implements WebMvcConfigurer {
     mr.setMaxUploadSizePerFile(5000000);
     return mr;
   }
-  
+
   // @MatrixVariable 애노테이션 처리를 활성화시킨다.
   @Override
   public void configurePathMatch(PathMatchConfigurer configurer) {
@@ -38,9 +44,16 @@ public class AppWebConfig implements WebMvcConfigurer {
     configurer.setUrlPathHelper(helper);
   }
 
+  // 프론트 컨트롤러에 적용할 인터셉터 설정하기
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
+    // 인터셉터 실행은 등록 순서이다.
 
+    // 자동 로그인을 수행하는 인터셉터를 삽입한다.
+    registry.addInterceptor(new AutoLoginInterceptor(memberService));
+
+    // 모든 "/app/*" 요청에 대해 로그인 여부를 검사하는 인터셉터를 삽입한다.
+    registry.addInterceptor(new AuthInterceptor());
   }
 }
 
